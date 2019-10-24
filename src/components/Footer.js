1e5303785f36fa598852efd69ae905b3013427eb
Choskas <choskasdelta@hotmail.com>
2019-10-24 15:30:49 -0500

    mobile changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const { Footer } = Layout;
 export default class FooterR extends Component {
     render() {
         return (
-            <div style={{width: 'auto'}}>
+            <div className='onMobileFooterDiv' style={{width: 'auto'}}>
                   
                   <Footer style={{background: 'black', color: 'white', display: 'flex', justifyContent: 'space-between', height: '20vh', width: '99vw', padding: '0px', paddingTop: '10px', margin: '0px' }} >
             <div style={{display: 'flex', justifyContent: 'space-around',width: '50%'}}>
--- src/components/Home.js
+++ src/components/Home.js
@@ -12,15 +12,15 @@ export default class Home extends Component {
             <Layout>
          <NavBar />
          </Layout>
-         <div style={{height: '85vh', width: 'auto', background: 'url("/images/Fondo corto 1.png")', backgroundRepeat: 'no-repeat', backgroundSize: '100vw 85vh' }}>
+         <div  style={{height: '85vh', width: 'auto', background: 'url("/images/Fondo corto 1.png")', backgroundRepeat: 'no-repeat', backgroundSize: '100vw 85vh' }}>
          <div style={{display: 'flex', flexDirection: 'row', paddingTop: '15vh'}}>
-         <div style={{display: 'flex', flexDirection: 'column', width:'20vw', marginLeft: '10vw'}}>
-             <h2 style={{fontSize: '32px', fontFamily: '"Archivo Black", sans-serif', color: 'white', textAlign: 'right'}}>My lastest projects</h2>
-             <p style={{fontSize: '32px', color: 'white', textAlign: 'right' }}>This are the latest projects <br></br> I made, and I'm happy to share with you</p>
-             <Link to='/projects'><img src='/images/Botón Proyectos.png' alt='boton' style={{width: '20vw', height: '10vh'}}></img></Link>
+         <div className='onMobileFirstDiv' style={{display: 'flex', flexDirection: 'column', width:'20vw', marginLeft: '10vw'}}>
+             <h2 className='onMobileFirstDiv' style={{fontSize: '32px', fontFamily: '"Archivo Black", sans-serif', color: 'white', textAlign: 'right'}}>My lastest projects</h2>
+             <p className='onMobileFirstDiv' style={{fontSize: '32px', color: 'white', textAlign: 'right' }}>This are the latest projects <br></br> I made, and I'm happy to share with you</p>
+             <Link to='/projects'><img className='onMobileButtonFirstDiv' src='/images/Botón Proyectos.png' alt='boton' style={{width: '20vw', height: '10vh'}}></img></Link>
          </div>
          <div>
-             <img src={'/images/Oscar .png'}  style={{width: '60vw',
+             <img className='onMobileMyImage'  src={'/images/Oscar .png'}  style={{width: '60vw',
     height: '70vh'}} alt='aquivalaimagen'/>
     </div> 
     </div>
@@ -63,21 +63,21 @@ I'm always learning.</p>
          <div style={{paddingTop: '5vh' ,height: 'auto', width: 'auto', background: 'url("/images/Fondo corto 2.png")', backgroundRepeat: 'repeat', backgroundSize: '100vw 85vh' }}>
          <h2 style={{fontSize: '38px', fontFamily: '"Archivo Black", sans-serif', color:'white'}}>Projects</h2>
          <div className='onMobileCardContainerHome' style={{display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'space-between', maxWidth: '100vw'}}>
-                <Card style={{borderRadius: '5px', width: '20rem',height:'68vh', background: 'black', border: '0px'}}>
+                <Card className='onMobileCardContainerProjects' style={{borderRadius: '5px', width: '20rem',height:'68vh', background: 'black', border: '0px'}}>
                 <img src={'/images/teach.png'} style={{width: '18rem', height: '25vh'}} alt={'teach'}></img>
                 <h2 style={{fontSize: '32px', fontFamily: '"Archivo Black", sans-serif', color:'white'}}>Teach and Learn</h2>
             <p style={{color: 'white', fontSize: '1.2rem'}}>This project was carried out in a week. It aimed to communicate students with teachers.
             </p>
                 </Card>
 
-                <Card style={{borderRadius: '5px', width: '20rem',height:'68vh', background: 'black', border: '0px'}}>
+                <Card className='onMobileCardContainerProjects' style={{borderRadius: '5px', width: '20rem',height:'68vh', background: 'black', border: '0px'}}>
                 <img src={'/images/DOM.png'} style={{width: '18rem', height: '25vh'}} alt={'dom'}></img>
                 <h2 style={{fontSize: '32px', fontFamily: '"Archivo Black", sans-serif', color:'white'}}>Phantom Ninja Ultra</h2>
             <p style={{color: 'white', fontSize: '1.2rem'}}>My first project as a developer is a Videogame just using canvas and Javascript.
             </p>
                 </Card>
 
-                <Card style={{borderRadius: '5px', width: '20rem',height:'68vh', background: 'black', border: '0px'}}>
+                <Card className='onMobileCardContainerProjects' style={{borderRadius: '5px', width: '20rem',height:'68vh', background: 'black', border: '0px'}}>
                 <img src={'/images/gamingcouple.png'} style={{width: '18rem', height: '25vh'}} alt={'gaming'}></img>
                 <h2 style={{fontSize: '32px', fontFamily: '"Archivo Black", sans-serif', color:'white'}}>The Gaming Couple</h2>
             <p style={{color: 'white', fontSize: '1.2rem'}}>MERN project based on E-Sports, using a REST-Full API.
